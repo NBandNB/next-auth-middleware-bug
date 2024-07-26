6e19dd68ccbe91c082a32b6180c47380f30bae2f
LotsOfBlochs <69881693+LotsOfBlochs@users.noreply.github.com>
2024-07-26 12:06:33 -0700

    And route handler as requested by response.

--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 
-export const {auth } = NextAuth(() => {
+export const {auth, handlers } = NextAuth(() => {
     return {
         providers: [],
         secret: "a16116c91c087f63a653852798e09202c46fa0e0",
